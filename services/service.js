66fbc28accd3acaebfc8dd40108a4c9be6bf9b06
Devin (they/them) <jinipier@iu.edu>
2023-04-25 10:38:25 -0400

    nav color
    aaaAAAAAAAAAAAAAAAAAAAAAAAAAASAAaaAAAA

--- services/service.js
+++ services/service.js
@@ -1,7 +1,17 @@
 export function changeRoute(){
+    let navbar = document.getElementById("navigation");
     let hashTag = window.location.hash;
     let pageID = hashTag.replace("#", "");
     if (pageID != ""){
+        if(pageID == "home"){
+            $("nav").addClass("nav-white");
+            $("nav").removeClass("nav-black");
+            $("nav img").attr("src", "images/logo/logo-white.png");
+        }else{
+            $("nav").addClass("nav-black");
+            $("nav").removeClass("nav-white");
+            $("nav img").attr("src", "images/logo/logo-black.png");
+        }
         console.log(pageID);
         $.get(`pages/${pageID}.html`, function(data){
             console.log("data" + data);
